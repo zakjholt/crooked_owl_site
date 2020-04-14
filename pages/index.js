cf06b5d54c9c2ce64f4da1cf41f4b2150bceb5b0
Zak <zak.j.holt@gmail.com>
2020-04-14 18:24:31 -0400

    name in the site

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ const Home = () => (
   <section class="hero is-fullheight">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">🦉</h1>
-        <h2 class="subtitle">coming soon</h2>
+        <h1 class="title">🦉crooked owl</h1>
+        <h2 class="subtitle">coming soon to trees near you</h2>
       </div>
     </div>
   </section>
