a99703342de27376a4c8e1b46384f26ffeb96c6c
Zak <zak.j.holt@gmail.com>
2020-04-14 18:10:18 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Home = () => (
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">🦉</h1>
+        <h2 class="subtitle">coming soon</h2>
+      </div>
+    </div>
+  </section>
+);
+
+export default Home;
